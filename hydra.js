d4c149a2ef77758ef8d75244f0c48de55f19c6ee
Matthew.Varona <mjv379@nyu.edu>
2023-07-02 13:13:51 +0800

    donut tweaks...

--- hydra.js
+++ hydra.js
@@ -69,10 +69,10 @@ window.onload = function() {
     p5.draw = ()=>{
       p5.background(255,55);
       p5.stroke(0);
-      p5.strokeWeight(100);
+      p5.strokeWeight(height/10);
       
       p5.translate(p5.width/2,p5.height/2);
-      p5.ellipse(0,0,height/2,height/2);
+      p5.ellipse(0,0,height/2.5,height/2.5);
     }
 
     s0.init({src: p5.canvas})
